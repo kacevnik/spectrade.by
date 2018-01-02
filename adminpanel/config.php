0b9ad37fdb9fe77a24c6b7991fd5ae1a7fead428
kacevnik <kacevnik@yandex.ru>
2018-01-02 11:17:38 +0300

    Установка минимальной безопасности

--- adminpanel/config.php
+++ adminpanel/config.php
@@ -1,18 +1,18 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://spectrade.by/admin/');
+define('HTTP_SERVER', 'http://spectrade.by/adminpanel/');
 define('HTTP_CATALOG', 'http://spectrade.by/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://spectrade.by/admin/');
+define('HTTPS_SERVER', 'http://spectrade.by/adminpanel/');
 define('HTTPS_CATALOG', 'http://spectrade.by/');
 
 // DIR
-define('DIR_APPLICATION', '/home/user1141394/www/spectrade.by/admin/');
+define('DIR_APPLICATION', '/home/user1141394/www/spectrade.by/adminpanel/');
 define('DIR_SYSTEM', '/home/user1141394/www/spectrade.by/system/');
 define('DIR_IMAGE', '/home/user1141394/www/spectrade.by/image/');
-define('DIR_LANGUAGE', '/home/user1141394/www/spectrade.by/admin/language/');
-define('DIR_TEMPLATE', '/home/user1141394/www/spectrade.by/admin/view/template/');
+define('DIR_LANGUAGE', '/home/user1141394/www/spectrade.by/adminpanel/language/');
+define('DIR_TEMPLATE', '/home/user1141394/www/spectrade.by/adminpanel/view/template/');
 define('DIR_CONFIG', '/home/user1141394/www/spectrade.by/system/config/');
 define('DIR_CACHE', '/home/user1141394/www/spectrade.by/system/storage/cache/');
 define('DIR_DOWNLOAD', '/home/user1141394/www/spectrade.by/system/storage/download/');
